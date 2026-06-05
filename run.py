e6badf18ddb77976b9d87617aaebe185ff421cee
Madhu123456-ai <madhu.assistanz@gmail.com>
2026-06-05 11:27:10 +0530

    run.py

--- run.py
+++ run.py
@@ -3,8 +3,3 @@
 from app import create_app
 
 app = create_app()
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5050))
-    app.run(host="0.0.0.0", port=port, debug=True)
